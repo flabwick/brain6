73eeea624370a2175afdba1e3b69b7f94380c47d
Flabwick <james.chadwick.official@gmail.com>
2025-08-04 01:35:23 +0000

    lots of general setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Hello from Clarity!</h1>
+      <p>If you can see this, React is working.</p>
+    </div>
+  );
+}
+
+export default App;
